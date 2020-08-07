d1e3021a8323fd4481cb9811f940530dc4a080a7
Brennan Newton <brennanmnewton@gmail.com>
2020-08-07 17:00:50 -0400

    Remove build folder from .gitignore

---
+++ build/precache-manifest.5fbde9f5b3c386ab3eb2c20a6dfe3360.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "282f9ff1637c25f7e021be18b9a9320c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "792a42e58012e0b4cd66",
+    "url": "/static/css/2.92a0e85f.chunk.css"
+  },
+  {
+    "revision": "bdb027bd5eaadd9bf41c",
+    "url": "/static/css/main.47d8ddd7.chunk.css"
+  },
+  {
+    "revision": "792a42e58012e0b4cd66",
+    "url": "/static/js/2.d23fede6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.d23fede6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdb027bd5eaadd9bf41c",
+    "url": "/static/js/main.36591e16.chunk.js"
+  },
+  {
+    "revision": "707d14ce1aec858e1091",
+    "url": "/static/js/runtime-main.0344e8d6.js"
+  },
+  {
+    "revision": "79eceac5e21164149d219dd0bf217621",
+    "url": "/static/media/Necklace_TP_Opacity.79eceac5.png"
+  },
+  {
+    "revision": "7059acee3b9d8ea9744c26ab45135a8b",
+    "url": "/static/media/SpecialElite-Regular.7059acee.ttf"
+  },
+  {
+    "revision": "4b55699b63c80aab18fb000e6e92f88d",
+    "url": "/static/media/fb_ivory.4b55699b.png"
+  },
+  {
+    "revision": "b6adf422a935cc0a3b8be2330c7289d5",
+    "url": "/static/media/ig_ivory.b6adf422.png"
+  },
+  {
+    "revision": "54e517a920a53046a271853e275c8eb8",
+    "url": "/static/media/ivory_tp.54e517a9.png"
+  }
+]);
