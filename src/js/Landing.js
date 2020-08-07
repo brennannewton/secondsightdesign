6c533de250c1ffd1f02e09b21efe58436a6ef2cf
Brennan Newton <brennanmnewton@gmail.com>
2020-08-07 16:52:14 -0400

    Add landing component with hero image and title

---
+++ src/js/Landing.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '../css/Landing.css'
+
+export default class Landing extends React.Component {
+    render() {
+        return (
+            <div className="Landing-Cmpnt">
+                <h1>Second Sight Design</h1>
+            </div>
+        )
+    }
+}
